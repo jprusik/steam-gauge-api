6f518d592ff4a4b6ae17c76fed5a8bfb1b3a39ab
Jon Prusik <jprusik@classynemesis.com>
2018-09-02 13:58:16 -0400

    add config example

---
+++ app/config-example.py
@@ -0,0 +1,15 @@
+ADMIN_USERS = []
+API_KEY = '<your api key>'
+APP_DIRECTORY = '/app'
+PYTHON_DIRECTORY = '/usr/bin/python'
+REACT_JS_DOMAIN = '//localhost'
+REACT_JS_PORT = '3000'
+SESSION_EXPIRE_MINUTES = 720
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://<db user>:<db password>@<db domain>:<port>/<schema>'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Flask-specific keys
+DEBUG = True # Reminder: Don't leave debug on in production!!
+SECRET_KEY = '<app secret>'
+JSON_AS_ASCII = False
+#####################
